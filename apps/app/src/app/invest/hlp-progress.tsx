086fd60f2b2b2f86f97c2ffc5cb06566f825cea6
i-xtsu-sixyou-ken-mei <9bmf4by27w@privaterelay.appleid.com>
2026-09-02 16:02:54 +0900

    feat: add HLP progress route

---
+++ apps/app/src/app/invest/hlp-progress.tsx
@@ -0,0 +1,7 @@
+import type { ReactElement } from 'react';
+
+import { HlpProgressScreen } from '@/screens/invest/HlpProgressScreen';
+
+export default function HlpProgressRoute(): ReactElement {
+  return <HlpProgressScreen />;
+}
