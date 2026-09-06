1c47cf33ad95189f4f86c906728df2264540bd4e
i-xtsu-sixyou-ken-mei <9bmf4by27w@privaterelay.appleid.com>
2026-09-06 10:25:04 +0900

    Wire podcast abandon route into Vercel app

--- apps/control-center/api/index.ts
+++ apps/control-center/api/index.ts
@@ -1,11 +1,16 @@
 import { createControlCenterApp } from '../src/server/app.js';
 import { readControlCenterConfig } from '../src/server/config/env.js';
 import { initSentry } from '../src/server/observability/sentry.js';
+import { registerPodcastAbandonRoute } from '../src/server/register-podcast-abandon.js';
 
 initSentry(process.env);
 
-export default createControlCenterApp({
-  config: readControlCenterConfig(),
+const config = readControlCenterConfig();
+const app = createControlCenterApp({
+  config,
   serveClient: false,
   allowCostSync: false,
 });
+registerPodcastAbandonRoute(app, { config });
+
+export default app;
