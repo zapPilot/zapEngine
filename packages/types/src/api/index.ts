010c6c84997493d94685cb305a75eab2c9b41174
i-xtsu-sixyou-ken-mei <9bmf4by27w@privaterelay.appleid.com>
2026-09-13 19:01:47 +0900

    feat(types): export routed deposit schema

--- packages/types/src/api/index.ts
+++ packages/types/src/api/index.ts
@@ -73,6 +73,7 @@ export type Nullable<T> = T | null;
 
 export * from './marketDashboard.js';
 export * from './deposit.js';
+export * from './routed-deposit.js';
 export * from './withdraw.js';
 export * from './privy-wallet.js';
 export * from './execution-review.js';
